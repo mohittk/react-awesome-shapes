feac20b06f0192e634d3948f3c226568997dedfb
mohittk <mvk1407@gmail.com>
2022-02-20 10:09:41 +0530

    test

--- src/lib/shapes/square.tsx
+++ src/lib/shapes/square.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { BaseShape } from './base';
+import styled from "@emotion/styled";
+import {BaseShape} from "./base";
 
 type SquareProps = {
-  size: string | string[];
-  color: string;
-  top?: string | string[];
-  left?: string | string[];
-  right?: string | string[];
-  bottom?: string | string[];
-  className?: string;
-  zIndex?: string;
-  breakpoints?: number[];
-  position?: string;
-};
+    size : string | string[]
+    color : string
+    top? : string | string[]
+    left?: string | string[]
+    right?: string | string[]
+    bottom?: string | string[]
+    className?: string
+    zIndex?: string
+    breakpoints?: number[]
+    position?: string
+
+}
+
 
 export const Square = ({
-  size,
-  color,
-  top = ``,
-  position = `absolute`,
-  left = ``,
-  right = ``,
-  bottom = ``,
-  className = `anim-shape-circle`,
-  zIndex = `-1`,
-  breakpoints = [600, 900, 1200]
-}: SquareProps) => {
-  const StyledSquare = styled('div')`
+    size,
+    color,
+    top=``,
+    position=`absolute`,
+    left = ``,
+    right = ``,
+    bottom = ``,
+    className = `anim-shape-square`,
+    zIndex = `-1`,
+    breakpoints=[600, 900, 1200] }: SquareProps) => {
+
+const StyledSquare= styled('div')`
     position: inherit;
     height: inherit;
     width: inherit;
@@ -37,22 +38,21 @@ export const Square = ({
     left: inherit;
     bottom: inherit;
     z-index: inherit;
-  `;
+`
 
-  return (
-    <BaseShape
-      bottom={bottom}
-      breakpoints={breakpoints}
-      className={className}
-      height={size}
-      left={left}
-      position={position}
-      right={right}
-      top={top}
-      width={size}
-      zIndex={zIndex}
-    >
-      <StyledSquare className={className} />
-    </BaseShape>
-  );
-};
+return <BaseShape
+          height={size}
+          width={size}
+          top={top}
+          left={left}
+          right={right}
+          bottom={bottom}
+          zIndex={zIndex}
+          className={className}
+          position={position}
+          breakpoints={breakpoints}>
+        <StyledSquare className={className} />
+</BaseShape>;
+}
+
+
